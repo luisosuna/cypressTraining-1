715f45ce84d36c7dbea740ee96a0c4a4442225f0
Fernando Anda <fernandoanda11@gmail.com>
2025-05-19 14:18:03 -0500

    apple_iframes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,18 @@
-describe('template spec', () => {
+describe('Work with iFrames', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-    cy.contains('Kitchen Sink')
+    cy.visit('https://www.apple.com/')
+    cy.get('#globalnav-menubutton-link-bag').click()
+    cy.contains('Sign in').click()
+    cy.wait(3000)
+    getIframeDocument().find('.form-checkbox-indicator').click()
+
+    
   })
+
 })
+
+const getIframeDocument = () => {
+  return cy
+  .get('#aid-auth-widget-iFrame')
+  .its('0.contentDocument').should('exist')
+}
