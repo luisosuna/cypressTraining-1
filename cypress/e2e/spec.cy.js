819f16148f639c87eada06665b6c4b804205480b
Luis <luis.osuna@unosquare.com>
2025-06-26 14:26:40 -0600

    xxx

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -11,8 +11,12 @@ describe('Work with iFrames', () => {
 })
 
 const getIframeBody = () => {
+  // Get the iframe > document > body
+  // and retry until the body element is not empty
   return cy
-    .get('#aid-auth-widget-iFrame')
+    .get('#aid-auth-widget-iFrame', { timeout: 10000 }) // Increased timeout for iframe loading
     .its('0.contentDocument.body').should('not.be.empty')
-    .then(cy.wrap)
-}
+    // Wraps "body" DOM element to allow
+    // chaining more Cypress commands, like ".find(...)"
+    .then(cy.wrap);
+};
